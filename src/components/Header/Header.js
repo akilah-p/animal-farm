a667be8f9c21b201e536014d60301bb4e3ccb688
Akilah P <atpjun@gmail.com>
2023-01-09 20:49:51 -0400

    rendered animals and background

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Header.css';
-export default function Header(booger) {
-  const displayText = booger.text ? booger.text : 'This is my Header';
-  return <h1>{displayText}</h1>;
+export default function Header() {
+  return <header>Alchemy Animal Farm</header>;
 }

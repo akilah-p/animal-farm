34c0b64a090825550ea66801c932d409a45e136e
Akilah P <atpjun@gmail.com>
2023-01-09 20:05:11 -0400

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
-export default function Header() {
-  return <header>Alchemy Animal Farm</header>;
+export default function Header(booger) {
+  const displayText = booger.text ? booger.text : 'This is my Header';
+  return <h1>{displayText}</h1>;
 }
